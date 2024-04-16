5e62258994046c632eb09bb03d742543f3da3628
mskogli <mathsko@stud.ntnu.no>
2024-04-16 12:33:40 +0200

    config

--- aerial_gym/envs/base/aerial_robot_with_obstacles_config.py
+++ aerial_gym/envs/base/aerial_robot_with_obstacles_config.py
@@ -97,6 +97,39 @@ class AerialRobotWithObstaclesCfg(BaseConfig):
             np.pi / 4.0,
         ]  # scale the input to the controller from -1 to 1 for each dimension
 
+    class control:
+        """
+        Control parameters
+        controller:
+            lee_position_control: command_actions = [x, y, z, yaw] in environment frame scaled between -1 and 1
+            lee_velocity_control: command_actions = [vx, vy, vz, yaw_rate] in vehicle frame scaled between -1 and 1
+            lee_attitude_control: command_actions = [thrust, roll, pitch, yaw_rate] in vehicle frame scaled between -1 and 1
+        kP: gains for position
+        kV: gains for velocity
+        kR: gains for attitude
+        kOmega: gains for angular velocity
+        """
+
+        controller = "lee_velocity_control"  # or "lee_velocity_control" or "lee_attitude_control"
+        kP = [0.8, 0.8, 1.0]  # used for lee_position_control only
+        kV = [0.5, 0.5, 0.4]  # used for lee_position_control, lee_velocity_control only
+        kR = [
+            3.0,
+            3.0,
+            1.0,
+        ]  # used for lee_position_control, lee_velocity_control and lee_attitude_control
+        kOmega = [
+            0.5,
+            0.5,
+            1.20,
+        ]  # used for lee_position_control, lee_velocity_control and lee_attitude_control
+        scale_input = [
+            2.0,
+            1.0,
+            1.0,
+            np.pi / 4.0,
+        ]  # scale the input to the controller from -1 to 1 for each dimension
+
     class robot_asset:
         file = "{AERIAL_GYM_ROOT_DIR}/resources/robots/quad/model.urdf"
         name = "aerial_robot"  # actor name
@@ -126,7 +159,6 @@ class AerialRobotWithObstaclesCfg(BaseConfig):
 
         collapse_fixed_joints = True
         fix_base_link = True
-        disable_gravity = False
         links_per_asset = 1
         set_whole_body_semantic_mask = False
         set_semantic_mask_per_link = False
@@ -136,7 +168,7 @@ class AerialRobotWithObstaclesCfg(BaseConfig):
 
     class thin_asset_params(asset_state_params):
         num_assets = 10
-        num_dynamic = 2
+        num_dynamic_assets = 2
 
         collision_mask = 1  # objects with the same collision mask will not collide
 
@@ -148,6 +180,7 @@ class AerialRobotWithObstaclesCfg(BaseConfig):
             -1000.0,
             -1000.0,
         ]  # if > -900, use this value instead of randomizing   the ratios
+
         min_euler_angles = [-np.pi, -np.pi, -np.pi]  # min euler angles
         max_euler_angles = [np.pi, np.pi, np.pi]  # max euler angles
 
@@ -158,7 +191,6 @@ class AerialRobotWithObstaclesCfg(BaseConfig):
         ]  # if > -900, use this value instead of randomizing
 
         collapse_fixed_joints = True
-        fix_base_link = True
         links_per_asset = 1
         set_whole_body_semantic_mask = True
         semantic_id = THIN_SEMANTIC_ID
@@ -167,8 +199,7 @@ class AerialRobotWithObstaclesCfg(BaseConfig):
         color = [170, 66, 66]
 
     class tree_asset_params(asset_state_params):
-        num_assets = 1
-        num_dynamic = 1
+        num_assets = 10
 
         collision_mask = 1  # objects with the same collision mask will not collide
 
@@ -191,7 +222,6 @@ class AerialRobotWithObstaclesCfg(BaseConfig):
         ]  # if > -900, use this value instead of randomizing
 
         collapse_fixed_joints = True
-        fix_base_link = True
         links_per_asset = 1
         set_whole_body_semantic_mask = False
         set_semantic_mask_per_link = True
@@ -200,11 +230,11 @@ class AerialRobotWithObstaclesCfg(BaseConfig):
         color = [70, 200, 100]
 
     class object_asset_params(asset_state_params):
-        num_assets = 36
-        num_dynamic = 12
+        num_assets = 35
+        num_dynamic_assets = 15
 
         max_position_ratio = [0.95, 0.95, 0.95]  # min position as a ratio of the bounds
-        min_position_ratio = [0.05, 0.05, 0.05]  # max position as a ratio of the bounds
+        min_position_ratio = [0.05, 0.05, 0.2]  # max position as a ratio of the bounds
 
         specified_position = [
             -1000.0,
